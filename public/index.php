0df4c47a0c7f1fb1a1dc940be998c5e2498cb2a7
Rumakashy <vincent.plasson@outlook.fr>
2017-10-11 11:57:57 +0200

    Modif' page Accueil + Shop css

--- public/index.php
+++ public/index.php
@@ -7,10 +7,8 @@ include '../src/functions.php';
 
 <!-- Start Banner -->
 
-<div class="row">
-	<div class="banner">
-		<img src="http://via.placeholder.com/200x550" alt="#" />
-	</div>
+<div class="row col-xs-offset-1 col-xs-10 banner">
+    <img src="http://via.placeholder.com/1920x500" alt="#" />
 	<p class="col-xs-offset-8 col-xs-4">
 		<cite>
 			<span class="fa fa-quote-left" aria-hidden="true"></span>
@@ -23,75 +21,85 @@ include '../src/functions.php';
 
 <!-- Start Actu / Img Insta, Blog -->
 
-<h2>Notre Actualité</h2>
-<hr />
-<div class="row">
-		<div class="col-xs-4">
-			<img src="http://via.placeholder.com/200x200" alt="">
+<div class="row col-xs-offset-1 col-xs-10 actu">
+    <h2>Notre Actualité</h2>
+    <hr />
+    <div class="col-xs-4 imgblog">
+        <img src="http://via.placeholder.com/800x800" alt="">
+    </div>
+    <div class="col-xs-4 imginsta">
+        <div class="row">
+            <img class="col-xs-6" src="http://via.placeholder.com/550x550" alt="">
+            <img class="col-xs-6" src="http://via.placeholder.com/250x250" alt="">
+        </div>
+    </div>
+    <div class="col-xs-4 imgblog">
+        <img src="http://via.placeholder.com/500x500" alt="">
+    </div>
+</div>
+<div class="row col-xs-offset-1 col-xs-10">
+    <div class="col-xs-4 imginsta">
+        <div class="row">
+            <img class="col-xs-6" src="http://via.placeholder.com/250x250" alt="">
+            <img class="col-xs-6" src="http://via.placeholder.com/250x250" alt="">
+        </div>
+    </div>
+		<div class="col-xs-4 imgblog">
+			<img class="imgup" src="http://via.placeholder.com/500x500" alt="">
 		</div>
-		<div id="imginsta" class="col-xs-4">
-			<img src="http://via.placeholder.com/100x100" alt="">
-			<img src="http://via.placeholder.com/100x100" alt="">
-		</div>
-		<div class="col-xs-4">
-			<img src="http://via.placeholder.com/200x200" alt="">
-		</div>
-
-		<div class="col-xs-4">
-			<img src="http://via.placeholder.com/100x100" alt="">
-			<img src="http://via.placeholder.com/100x100" alt="">
-		</div>
-		<div class="col-xs-4">
-			<img src="http://via.placeholder.com/200x200" alt="">
-		</div>
-		<div class="col-xs-4">
-			<img src="http://via.placeholder.com/100x100" alt="">
-			<img src="http://via.placeholder.com/100x100" alt="">
-		</div>			
+    <div class="col-xs-4 imginsta">
+        <div class="row">
+            <img class="col-xs-6" src="http://via.placeholder.com/250x250" alt="">
+            <img class="col-xs-6" src="http://via.placeholder.com/250x250" alt="">
+        </div>
+    </div>
 </div>
 <!-- End Actu / Img Insta, Blog -->
 
 <!-- Start About us -->
-
-<h2>Qui sommes-nous</h2>
-<hr />
-<div class="row">
-	<div class="col-xs-offset-1 col-xs-4">
-			<img src="http://via.placeholder.com/300x200" alt="">
-	</div>
-	<div class="col-xs-offset-1 col-xs-5">
-		<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur arcu vel justo posuere, et vehicula nibh interdum. Aliquam viverra ornare fringilla. Cras blandit et sem quis iaculis. Nullam placerat mi sed mauris finibus, sed ullamcorper nunc rhoncus. Proin dapibus lacus sit amet eros accumsan porta. Aliquam at pellentesque odio. Curabitur dictum malesuada sem non tincidunt. Morbi semper id risus finibus finibus.</p>
-	</div>
+<div class="row col-xs-offset-1 col-xs-10 aboutus">
+    <h2>Qui sommes-nous</h2>
+    <hr />
+        <img class="col-xs-offset-1 col-xs-4" src="http://via.placeholder.com/800x800" alt="">
+        <p class="col-xs-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur arcu vel justo posuere, et vehicula nibh interdum. Aliquam viverra ornare fringilla. Cras blandit et sem quis iaculis. Nullam placerat mi sed mauris finibus, sed ullamcorper nunc rhoncus. Proin dapibus lacus sit amet eros accumsan porta. Aliquam at pellentesque odio. Curabitur dictum malesuada sem non tincidunt. Morbi semper id risus finibus finibus.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur arcu vel justo posuere, et vehicula nibh interdum. Aliquam viverra ornare fringilla. Cras blandit et sem quis iaculis. Nullam placerat mi sed mauris finibus, sed ullamcorper nunc rhoncus. Proin dapibus lacus sit amet eros accumsan porta. Aliquam at pellentesque odio. Curabitur dictum malesuada sem non tincidunt. Morbi semper id risus finibus finibus.
+        </p>
 </div>
 <!-- End About us -->
 
 <!-- Start Partner -->
 
-<h2>Où nous trouvez</h2>
-<hr />
-<div class="row">
-	<div class="col-xs-2">
-		<img src="http://via.placeholder.com/100x100" alt="#" />
-	</div>
-	<div class="col-xs-2">
-		<p>
-			<a href="#">Partenaire 1</a><br/>
-			<a href="#">www.adresse1.com</a>
-		</p>
-	</div>
-	<div class="col-xs-2">
-		<img src="http://via.placeholder.com/100x100" alt="#" />
-	</div>
-	<div class="col-xs-2">
-		<p><a href="#">Partenaire 1</a><br><a href="#">www.adresse1.com</a></p>
-	</div>
-	<div class="col-xs-2">
-			<img src="http://via.placeholder.com/100x100" alt="#" />
-	</div>
-	<div class="col-xs-2">
-		<p><a href="#">Partenaire 1</a><br><a href="#">www.adresse1.com</a></p>
-	</div>
+<div class="row col-xs-offset-1 col-xs-10 partner">
+    <h2>Où nous trouvez</h2>
+    <hr />
+        <div class="col-xs-offset-1 col-xs-1 imgpartner">
+            <img src="http://via.placeholder.com/500x500" alt="#" />
+        </div>
+        <div class="col-xs-2 txtpartner">
+            <p>
+                <a class="namepartner" href="#">Partenaire 1</a><br/>
+                <a class="urlpartner" href="#">www.adresse1.com</a>
+            </p>
+        </div>
+        <div class="col-xs-offset-1 col-xs-1 imgpartner">
+            <img src="http://via.placeholder.com/500x500" alt="#" />
+        </div>
+        <div class="col-xs-2 txtpartner">
+            <p>
+                <a class="namepartner" href="#">Partenaire 1</a><br>
+                <a class="urlpartner" href="#">www.adresse1.com</a>
+            </p>
+        </div>
+        <div class="col-xs-offset-1 col-xs-1 imgpartner">
+                <img src="http://via.placeholder.com/500x500" alt="#" />
+        </div>
+        <div class="col-xs-2 txtpartner">
+            <p>
+                <a class="namepartner" href="#">Partenaire 1</a><br>
+                <a class="urlpartner" href="#">www.adresse1.com</a>
+            </p>
+        </div>
 </div>
 <!-- End Partner -->
 
--- public/shop.php
+++ public/shop.php
@@ -1,67 +1,105 @@
 <?php include 'header.php'; ?>
 
-<button href="#" class="btn btn-default btn">Voir tous nos produits en vente</button>
+<div class="row txtshop">
+    <p class="col-xs-offset-2 col-xs-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur arcu vel justo posuere, et vehicula nibh interdum. Aliquam viverra ornare fringilla. Cras blandit et sem quis iaculis.
+    </p>
+</div>
 
-<div class="row">
-	<div class="col-xs-4">
-		<div class="thumbnail">
-	      <img src="http://via.placeholder.com/200x200" alt="">
-	      <div class="caption">
-	        <h3>Article 1</h3>
-	        <p><a href="#" class="btn btn-default" role="button">Button</a></p>
-	      </div>
-	    </div>
-	</div>
-	<div class="col-xs-4">
-		<div class="thumbnail">
-	      <img src="http://via.placeholder.com/200x200" alt="">
-	      <div class="caption">
-	        <h3>Article 2</h3>
-	        <p><a href="#" class="btn btn-default" role="button">Button</a></p>
-	      </div>
-	    </div>
-	</div>
-	<div class="col-xs-4">
-		<div class="thumbnail">
-	      <img src="http://via.placeholder.com/200x200" alt="">
-	      <div class="caption">
-	        <h3>Article 3</h3>
-	        <p><a href="#" class="btn btn-default" role="button">Button</a></p>
-	      </div>
-	    </div>
-	</div>
+<div class="row btnshoptop">
+    <button href="#" class="col-xs-offset-2 btn btn-default btntop">Voir tous nos produits en vente</button>
 </div>
 
 <div class="row">
-	<div class="col-xs-4">
+    <div class="col-xs-offset-2 col-xs-2">
 		<div class="thumbnail">
-	      <img src="http://via.placeholder.com/200x200" alt="">
-	      <div class="caption">
-	        <h3>Article 4</h3>
-	        <p><a href="#" class="btn btn-default" role="button">Button</a></p>
-	      </div>
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 1</h3>
+                <span class="price">00€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>                </p>
+            </div>
 	    </div>
 	</div>
-	<div class="col-xs-4">
+	<div class="col-xs-2">
 		<div class="thumbnail">
-	      <img src="http://via.placeholder.com/200x200" alt="">
-	      <div class="caption">
-	        <h3>Article 5</h3>
-	        <p><a href="#" class="btn btn-default" role="button">Button</a></p>
-	      </div>
+            <img src="http://via.placeholder.com/800x800" alt="">
+            <div class="caption">
+                <h3>Article 2</h3>
+                <span class="price">00.99€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>                </p>
+            </div>
 	    </div>
 	</div>
-	<div class="col-xs-4">
+	<div class="col-xs-2">
 		<div class="thumbnail">
-	      <img src="http://via.placeholder.com/200x200" alt="">
-	      <div class="caption">
-	        <h3>Article 6</h3>
-	        <p><a href="#" class="btn btn-default" role="button">Button</a></p>
-	      </div>
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 3</h3>
+                <span class="price">00€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>                </p>
+            </div>
 	    </div>
 	</div>
+    <div class="col-xs-2">
+        <div class="thumbnail">
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 3</h3>
+                <span class="price">00€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 
-<input id="btn2" type="submit" name="" value="Voir tous nos produits en vente">
+<div class="row">
+    <div class="col-xs-offset-2 col-xs-2">
+        <div class="thumbnail">
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 1</h3>
+                <span class="price">00€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <div class="thumbnail">
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 2</h3>
+                <span class="price">00€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <div class="thumbnail">
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 3</h3>
+                    <span class="price">00€</span>
+                    <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <div class="thumbnail">
+            <img src="http://via.placeholder.com/500x500" alt="">
+            <div class="caption">
+                <h3>Article 3</h3>
+                <span class="price">00€</span>
+                <a href="#" class="btn btn-default col-xs-offset-1" role="button"><span class="glyphicon glyphicon-tag"></span></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row btnshopbot">
+    <button href="#" class="col-xs-offset-8 btn btn-default btn">Voir tous nos produits en vente</button>
+</div>
 
 <?php include 'footer.php'; ?>
