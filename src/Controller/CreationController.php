b94c3637e27d5f1e3a05407f0921722c90a0a559
RequinC <em.leviavant@gmail.com>
2017-10-27 14:18:12 +0200

    fonction unlink qui marche sur le delete

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -77,10 +77,17 @@ class CreationController extends Controller
         if (!empty($_POST['id'])) {
             $creationManager = new CreationManager();
             $creation = $creationManager->find($_POST['id']);
+            $picture = $creation->getUrlPicture();
             $creationManager->delete($creation);
+
+            if (file_exists('uploads/'. $picture)) {
+
+                unlink('uploads/'. $picture);
+            }
             header('Location: admin.php?route=adminShop');
         }
     }
+
 
     public function listAction()
     {
@@ -92,92 +99,62 @@ class CreationController extends Controller
         ]);
     }
 
-
-    public function addOrUpdateAction(Creation $creation)
-    {
-        // traitement des erreurs éventuelles
-        $creation->setTitle($_POST['title']);
-        $creation->setPrice($_POST['price']);
-        $creation->setUrlPicture($_POST['url_picture']);
-        $creation->setUrlEtsy($_POST['url_etsy']);
-
-        return $creation;
-
-    }
-
-    public function checkError()
-    {
-        if (empty($_POST['title'])) {
-            $errors[] = 'Le champs titre ne doit pas être vide';
-        }
-
-        if (empty($_POST['price'])) {
-            $errors[] = 'Le champs prix ne doit pas être vide';
-        }
-
-        if (empty($_POST['url_picture'])) {
-            $errors[] = 'vous devez ajouter une photo';
-        }
-
-        if (empty($_POST['url_etsy'])) {
-    $errors[] = 'Vous devez ajouter un lien Etsy';
-        }
-
-        return $errors;
-    }
-
-
     public function updateAction()
     {
         $errors = [];
+        $success = [];
+        $uploadErrors = [];
         $creationManager = new CreationManager();
 
         if (!empty($_POST)) {
             $creation = $creationManager->find($_POST['id']);
-            $creationManager->addOrUpdateAction($creation);
+//set ton objet creation
 
-            $errors = $this->checkError();
 
-            if (empty($errors)) {
-
-                $creationManager = new CreationManager();
-                $creationManager->update($creation);
-
-                header('Location: admin.php?route=adminShop');
-
+            if (empty($_POST['title'])) {
+                $errors[] = "Veuillez ajouter un titre";
             }
 
+            if (empty($_POST['price'])) {
+                $errors[] = 'Veuillez ajouter un prix';
+            }
 
+            if (empty($_POST['url_etsy'])) {
+                $errors[] = 'Veuillez ajouter un lien Etsy';
+            }
+
+            if (empty($_FILES['url_picture'])) {
+                $errors[] = 'Veuillez ajouter une photo';
+            }
+
+            $creation->setTitle($_POST['title']);
+            $creation->setPrice($_POST['price']);
+            $creation->setUrlEtsy($_POST['url_etsy']);
+
+
+            if (empty($errors)) {
+                $uploadManager = new UploadManager($_FILES);
+                $uploadErrors = $uploadManager->fileUpload();
+                if (empty($uploadErrors)) {
+                    $creation->setUrlPicture($uploadManager->getUrlPicture());
+
+                    $creationManager = new CreationManager();
+                    $creationManager->update($creation);
+                    $success [] = 'L\'objet a bien été modifié';
+                }
+            }
+            $allErrors = array_merge($errors, $uploadErrors);
+            $listCreations = $creationManager->findAll();
+
+            return $this->twig->render('Admin/Shop/adminShopList.html.twig', [
+                'errors' => $allErrors,
+                'success' => $success,
+                'creations' => $listCreations]);
         } else {
             $creation = $creationManager->find($_GET['id']);
             return $this->twig->render('Admin/Shop/adminShopAddCreation.html.twig', [
-                'creation' => $creation,
-                'errors' => $errors,]);
-
+                'creation' => $creation
+            ]);
         }
     }
-
 }
-
-//function updateAction()
-//{
-//    $errors = [];
-//    $personManager = new PersonManager();
-//
-//    if (!empty($_POST)) {
-//        $person = $personManager->find($_POST['id']);
-//        $person = $this->addOrUpdateAction($person);
-//
-//        $errors = $this->checkError();
-//
-//        // si pas d'erreur, insert en bdd
-//        if (empty($errors)) {
-//
-//            $personManager = new PersonManager();
-//            $personManager->update($person);
-//
-//            header('Location: index.php?route=showAll');
-//        }
-//    } else {
-//        $person = $personManager->find($_GET['id']);
-//    }
