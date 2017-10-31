7a53b08d463b77a2c9249f22b2b5dd64f271d19a
Eoras <pauldsb@outlook.fr>
2017-10-31 10:10:44 +0100

    Gestion des partenaires, ADD and DELETE, restructuration du système d'upload et gestion des messages d'erreurs

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,6 +10,8 @@ namespace AtelierO\Controller;
 
 use AtelierO\Model\AboutUs;
 use AtelierO\Model\AboutUsManager;
+use AtelierO\Model\Partner;
+use AtelierO\Model\PartnerManager;
 use AtelierO\Service\UploadManager;
 
 class AdminController extends Controller
@@ -62,6 +64,41 @@ class AdminController extends Controller
             }
         }
 
+        if (isset($_POST['partnerName'])) {
+            $newPartner = new Partner();
+            $newPartner->setName($_POST['partnerName']);
+            $newPartner->setUrl($_POST['partnerUrl']);
+
+            if (empty($_POST['partnerName'])) {
+                $messages['danger'][] = "Vous devez rentrer un nom pour votre partenaire.";
+            }
+
+            if (empty($_POST['partnerUrl'])) {
+                $messages['danger'][] = "Vous devez renseigner une URL vers le site internet de votre partenaire";
+            }
+
+            if (empty($_FILES['partnerFile']['name'])) {
+                $messages['danger'][] = "Vous devez ajouter une image pour votre partenaire";
+            }
+
+            if (!empty($_FILES['partnerFile']['name'])) {
+                $inputFileName = "partnerFile";
+                $nameFile = "partner_" . uniqid();
+                $uploadDir = 'uploads/';
+                $acceptedExtension = ['jpg', 'jpeg', 'png', 'gif'];
+                $partnerFile = new UploadManager($_FILES['partnerFile']);
+                $messages = $partnerFile->fileUploadUnique($inputFileName, $nameFile, $uploadDir, $acceptedExtension);
+            }
+
+            if (empty($messages['danger'])) {
+                $newPartner->setUrlPicture($messages['partnerFile'][0]);
+                $partnerManager = new PartnerManager();
+                $partnerManager->add($newPartner);
+                $_SESSION['success'] = "partner";
+                header("Location: admin.php?route=adminAccueil");
+                exit();
+            }
+        }
 
         if (!empty($_SESSION['success'])) {
             if ('banner' == $_SESSION['success']) {
@@ -72,8 +109,21 @@ class AdminController extends Controller
                 $messages['success'][] = "Votre présentation a bien été mise à jour.";
                 session_destroy();
             }
+            if ('partner' == $_SESSION['success']) {
+                $messages['success'][] = "Votre partenaire a bien été ajouté.";
+                session_destroy();
+            }
+            if ('partnerDeleted' == $_SESSION['success']) {
+                $messages['success'][] = "Votre partenaire a bien été supprimé.";
+                session_destroy();
+            }
         }
 
+        if (!isset($newPartner)) {
+            $newPartner = null;
+        }
+        $partnerManager = new PartnerManager();
+        $partners = $partnerManager->findAll();
         $aboutManager = new AboutUsManager();
         $aboutUs = $aboutManager->findLast();
         return $this->twig->render('Admin/Accueil/adminAccueil.html.twig', [
@@ -81,7 +131,32 @@ class AdminController extends Controller
             'aboutUs' => $aboutUs,
             'route' => $_GET['route'],
             'aboutUsPost' => $aboutUsPost,
+            'partners' => $partners,
+            'newPartner' => $newPartner,
         ]);
+    }
+
+    public function deletePartnerAction()
+    {
+        $messages = [];
+        if (!empty($_POST['id'])) {
+
+            $partnerManager = new PartnerManager();
+            $partner = $partnerManager->find($_POST['id']);
+            $fileName = $partner->getUrlPicture();
+            $partnerManager->delete($partner);
+
+            $fichier = __DIR__ . "/../../public/uploads/" . $fileName;
+            if (file_exists($fichier)) {
+                if ($fichier != "." AND $fichier != ".." AND !is_dir($fichier)) {
+                    unlink($fichier);
+                }
+            }
+            $_SESSION['success'] = "partnerDeleted";
+            header('Location: admin.php?route=adminAccueil');
+        } else {
+            $messages['danger'][] = "Un problème est survenu lors de la suppression, veuillez réessayer.";
+        }
     }
 
     /*
@@ -92,9 +167,9 @@ class AdminController extends Controller
         $inputFileName = "banner";
         $nameFile = "banner";
         $uploadDir = 'images/banner/';
-
+        $acceptedExtension = ['jpg'];
         $uploadFile = new UploadManager($_FILES);
-        $messages = $uploadFile->fileUploadReplace($inputFileName, $nameFile, $uploadDir);
+        $messages = $uploadFile->fileUploadUnique($inputFileName, $nameFile, $uploadDir, $acceptedExtension);
 
         return $messages;
     }
@@ -104,9 +179,9 @@ class AdminController extends Controller
         $inputFileName = "aboutUsFile";
         $nameFile = "aboutUs";
         $uploadDir = 'images/aboutUs/';
-
+        $acceptedExtension = ['jpg'];
         $uploadFile = new UploadManager($_FILES);
-        $messages = $uploadFile->fileUploadReplace($inputFileName, $nameFile, $uploadDir);
+        $messages = $uploadFile->fileUploadUnique($inputFileName, $nameFile, $uploadDir, $acceptedExtension);
 
         return $messages;
     }
